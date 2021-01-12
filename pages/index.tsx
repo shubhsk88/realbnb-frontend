ffb293dab0811db4726e9418f04532bf64131d48
shubhsk88 <shubham88singh@gmail.com>
2021-01-13 04:43:17 +0530

    Fixing the linter issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import App from '../components/App'
-import InfoBox from '../components/InfoBox'
-import Header from '../components/Header'
-import Submit from '../components/Submit'
+
 import PostList, {
   ALL_POSTS_QUERY,
   allPostsQueryVars,
@@ -10,10 +8,7 @@ import { initializeApollo, addApolloState } from '../lib/apolloClient'
 
 const IndexPage = () => (
   <App>
-    <Header />
-    <InfoBox>ℹ️ This page shows how to use SSG with Apollo.</InfoBox>
-    <Submit />
-    <PostList />
+    
   </App>
 )
 
