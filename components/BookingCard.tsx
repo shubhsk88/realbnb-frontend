5db5d43eab573484e3c688dd68392698e29fadfc
shubhsk88 <shubham88singh@gmail.com>
2021-01-20 04:00:44 +0530

    Fixing the error in using useEffect:

--- components/BookingCard.tsx
+++ components/BookingCard.tsx
@@ -1,22 +1,35 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import { Box, BoxProps, Button, Select, Text } from "@chakra-ui/react";
+import differenceInCalendarDays from "date-fns/differenceInCalendarDays";
 import { DateRangePickerComponent } from "./DatePicker";
 
+export interface RangeProps {
+  start: Date | null;
+  end: Date | null;
+}
 export const BookingCard = ({ ...props }: BoxProps): ReactElement => {
+  const [rangeDates, setRangesDate] = useState<RangeProps>({
+    start: null,
+    end: null,
+  });
+  const numbeOfDays = useMemo(() => {
+    const { start, end } = rangeDates;
+  });
+
   return (
     <Box minWidth="400px" {...props}>
       <Box p={5} shadow="md" borderRadius="10px">
         <Box display="flex" my={4} alignItems="center">
           <Text fontSize="3xl" fontWeight="bold" color="primary">
-            $200 
+            $200
           </Text>
           <Text px={3} fontSize="xl" color="gray.500">
             /1 Night
           </Text>
         </Box>
-        <DateRangePickerComponent />
+        <DateRangePickerComponent setRange={setRangesDate} />
         <Button w="100%" colorScheme="gray">
-          Book Now
+          Book Now {JSON.stringify(rangeDates)}
         </Button>
       </Box>
     </Box>
--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -1,6 +1,12 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
-import React, { useState } from "react";
+import React, {
+  Dispatch,
+  SetStateAction,
+  useState,
+  useEffect,
+  ReactElement,
+} from "react";
 import { enGB } from "date-fns/locale";
 import {
   DateRangePicker,
@@ -9,15 +15,26 @@ import {
   Modifiers,
 } from "react-nice-dates";
 import "react-nice-dates/build/style.css";
-import { Box, HStack, Input } from "@chakra-ui/react";
+import { RangeProps } from "./BookingCard";
+import { HStack, Input } from "@chakra-ui/react";
 import { checkDisableDate, dateRanges } from "../utils/dateUtils";
 import { Wrapper } from "./common";
-export function DateRangePickerComponent() {
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
+
+export function DateRangePickerComponent({
+  setRange,
+}: {
+  setRange: Dispatch<SetStateAction<RangeProps>>;
+}): ReactElement {
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
   const modifier: Modifiers = {
     disabled: (date) => checkDisableDate(date, dateRanges),
   };
+  useEffect(() => {
+    if (startDate && endDate) {
+      setRange((prev) => ({ ...prev, start: startDate, end: endDate }));
+    }
+  }, [startDate, endDate]);
 
   return (
     <Wrapper>
