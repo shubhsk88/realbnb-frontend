9705c7956ae32f4a599c41c174f8435a52e95ea8
shubhsk88 <shubham88singh@gmail.com>
2021-01-28 20:35:29 +0530

    removing the key

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import { ApolloProvider } from "@apollo/client";
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ReactElement } from "react";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 
 import { useApollo } from "../lib/apolloClient";
 import theme from "../styles/theme";
@@ -12,6 +14,14 @@ import { PaymentProvider } from "../components/context/PaymentContext";
 export default function App({ Component, pageProps }: AppProps): ReactElement {
   const apolloClient = useApollo(pageProps.initialApolloState);
 
+  Sentry.init({
+    dsn: process.env.SENTRY_KEY,
+    integrations: [new Integrations.BrowserTracing()],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
   return (
     <PaymentProvider>
       <ChakraProvider theme={theme} resetCSS={true}>
