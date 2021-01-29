96ad3b62b264b8feb2aa1327c22d63a25c851bd5
shubhsk88 <shubham88singh@gmail.com>
2021-01-30 05:04:03 +0530

    Adding the like functionality

--- components/SavedListModal.tsx
+++ components/SavedListModal.tsx
@@ -2,11 +2,16 @@ import { ReactElement, useState } from "react";
 import { useReactiveVar } from "@apollo/client";
 import {
   Box,
+  Button,
   ButtonProps,
+  Divider,
+  FormControl,
+  FormLabel,
   Heading,
   HStack,
   Icon,
   IconButton,
+  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -14,15 +19,28 @@ import {
   ModalHeader,
   ModalOverlay,
   Text,
+  useToast,
   VStack,
 } from "@chakra-ui/react";
-import { AiOutlineHeart } from "react-icons/ai";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { HiPlus } from "react-icons/hi";
 
 import { isLoggedInVar } from "@/lib/cache";
-import { IconButtonOpaque, Image } from "@/components/common";
-import { useGetUserListsQuery, useUpdateListMutation } from "@/generated";
+import {
+  ButtonPrimary,
+  IconButtonClear,
+  IconButtonOpaque,
+  Image,
+} from "@/components/common";
+import {
+  useCreateListMutation,
+  useGetUserListsQuery,
+  useUpdateListMutation,
+} from "@/generated";
+import BeatLoader from "react-spinners/BeatLoader";
 import { AuthModal } from "./Auth/AuthModal";
+import { IoChevronBackSharp } from "react-icons/io5";
+import { useForm } from "react-hook-form";
 
 interface SavedListProps extends ButtonProps {
   roomId: string;
@@ -52,7 +70,7 @@ export const SavedListModal = ({
         aria-label="Save room"
         icon={
           <Icon
-            as={AiOutlineHeart}
+            as={liked ? AiFillHeart : AiOutlineHeart}
             boxSize={5}
             color={liked ? "red.400" : "white"}
           />
@@ -84,8 +102,27 @@ interface MyModalProps {
 
 const MyModal = ({ isOpen, onClose, roomId, liked }: MyModalProps) => {
   const { data, loading, error } = useGetUserListsQuery();
-
+  const [isNewOpen, setIsNewOpen] = useState<boolean>(false);
+  const toast = useToast();
+  const [formError, setFormError] = useState<string>("");
   const [onUpdateList] = useUpdateListMutation();
+  const { handleSubmit, register } = useForm();
+  const [onCreateList, { loading: formLoading }] = useCreateListMutation({
+    onCompleted: ({ createList }) => {
+      if (createList.ok) {
+        toast({
+          title: "Added to favourite successfully",
+          status: "success",
+          duration: 2000,
+          isClosable: true,
+        });
+        onClose();
+      } else {
+        setFormError("An Unknown error occured.Please try again");
+      }
+    },
+    refetchQueries: ["getRooms"],
+  });
 
   if (loading) return <div>loading</div>;
   if (error) return <div>{JSON.stringify(error)}</div>;
@@ -97,63 +134,114 @@ const MyModal = ({ isOpen, onClose, roomId, liked }: MyModalProps) => {
     onUpdateList({ variables: { id, roomId } });
     onClose();
   };
+  const onSubmit = (data: { name: string }) => {
+    onCreateList({
+      variables: {
+        name: data.name,
+        roomId,
+      },
+    });
+  };
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader textAlign="center">Save to list</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody px={0}>
-          <VStack as="ul" align="stretch">
-            <Box p={2}>
-              <HStack as="li" spacing={10}>
-                <IconButton
-                  w={20}
-                  h={20}
-                  icon={<Icon as={HiPlus} boxSize={10} color="white" />}
-                  aria-label="add new list"
-                  bgColor="gray.700"
-                  borderRadius="md"
-                />
-
-                <Heading as="h4" fontSize="md" fontWeight="medium">
-                  Create new list
-                </Heading>
-              </HStack>
+        <ModalHeader textAlign="center">
+          {isNewOpen ? (
+            <Box display="flex">
+              <IconButton
+                mr={12}
+                aria-label="Back"
+                bgColor="transparent"
+                onClick={() => setIsNewOpen(false)}
+                icon={<Icon as={IoChevronBackSharp} boxSize="5" />}
+              />
+              <Text justifySelf="center">Name the new list</Text>
             </Box>
+          ) : (
+            "Save to list"
+          )}
+        </ModalHeader>
 
-            {lists.map((list) => (
-              <Box
-                key={list.id}
-                p={2}
-                cursor="pointer"
-                aria-role="button"
-                _hover={{ backgroundColor: "gray.100" }}
-                onClick={() => addToList(list.id)}
-              >
+        <ModalCloseButton />
+        <Divider />
+        {!isNewOpen ? (
+          <ModalBody px={0} my={2}>
+            <VStack as="ul" align="stretch">
+              <Box p={2}>
                 <HStack as="li" spacing={10}>
-                  <Image
+                  <IconButton
                     w={20}
                     h={20}
-                    photo={list.rooms[0]?.photos[0]}
+                    icon={<Icon as={HiPlus} boxSize={10} color="white" />}
+                    aria-label="add new list"
+                    bgColor="gray.700"
                     borderRadius="md"
+                    onClick={() => setIsNewOpen(true)}
                   />
-                  <div>
-                    <Heading as="h4" fontSize="md" fontWeight="medium">
-                      {list.name}
-                    </Heading>
-                    <Text>
-                      {list.rooms.length
-                        ? list.rooms.length
-                        : "Nothing Saved Yet"}
-                    </Text>
-                  </div>
+
+                  <Heading as="h4" fontSize="md" fontWeight="medium">
+                    Create new list
+                  </Heading>
                 </HStack>
               </Box>
-            ))}
-          </VStack>
-        </ModalBody>
+
+              {lists.map((list) => (
+                <Box
+                  key={list.id}
+                  p={2}
+                  cursor="pointer"
+                  aria-role="button"
+                  _hover={{ backgroundColor: "gray.100" }}
+                  onClick={() => addToList(list.id)}
+                >
+                  <HStack as="li" spacing={10}>
+                    <Image
+                      w={20}
+                      h={20}
+                      photo={list.rooms[0]?.photos[0]}
+                      borderRadius="md"
+                    />
+                    <div>
+                      <Heading as="h4" fontSize="md" fontWeight="medium">
+                        {list.name}
+                      </Heading>
+                      <Text>
+                        {list.rooms.length
+                          ? list.rooms.length
+                          : "Nothing Saved Yet"}
+                      </Text>
+                    </div>
+                  </HStack>
+                </Box>
+              ))}
+            </VStack>
+          </ModalBody>
+        ) : (
+          <ModalBody>
+            <VStack as="form" onSubmit={handleSubmit(onSubmit)} spacing={10}>
+              <FormControl id="name" isRequired>
+                <FormLabel>Name</FormLabel>
+                <Input
+                  ref={register}
+                  name="name"
+                  type="text"
+                  placeholder="Name"
+                  focusBorderColor="primary"
+                />
+              </FormControl>
+              <ButtonPrimary
+                isLoading={formLoading}
+                spinner={<BeatLoader size={8} color="white" />}
+                type="submit"
+                w="full"
+              >
+                Create
+              </ButtonPrimary>
+            </VStack>
+          </ModalBody>
+        )}
       </ModalContent>
     </Modal>
   );
