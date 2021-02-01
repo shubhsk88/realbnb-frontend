6d71584827ab77d821df62fbe3f5c685c596c8c0
shubhsk88 <shubham88singh@gmail.com>
2021-02-01 23:37:01 +0530

    Completing the listing page

--- components/ListDetailsCard.tsx
+++ components/ListDetailsCard.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Divider,
   Heading,
   HStack,
   StatNumber,
@@ -12,33 +13,45 @@ import { IconPair } from "./common";
 
 export const ListDetailsCard = () => {
   return (
-    <Box
-      w="80%"
-      p={4}
-      cursor="pointer"
-      _hover={{ borderRadius: "lg", shadow: "md", backgroundColor: "gray.50" }}
-    >
-      <Box display="flex" alignItems="center" overflow="hidden">
-        <Box w="1000px" borderRadius="lg" h="200px" bg="gray.800" mr={4}></Box>
-        <VStack spacing={2} align="stretch" p={4}>
-          <Text fontSize="md" color="gray.500">
-            Small
-          </Text>
-          <Heading fontSize="lg">Hotel Title</Heading>
-          <Text isTruncated noOfLines={2}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-            cumque laboriosam, provident tempore dignissimos eaque voluptates
-            dolore incidunt! Pariatur debitis laborum reprehenderit accusamus
-            nulla soluta provident dolorem consequuntur omnis error.
-          </Text>
-          <Box display="flex" pt={4} justifyContent="space-between">
-            <IconPair icon={IoBedOutline}>6</IconPair>
-            <Text color="primary" fontWeight="bold" fontSize="3xl">
-              $6200
+    <>
+      <Box
+        w="80%"
+        p={4}
+        cursor="pointer"
+        _hover={{
+          borderRadius: "lg",
+          shadow: "md",
+          backgroundColor: "gray.50",
+        }}
+      >
+        <Box display="flex" alignItems="center" overflow="hidden">
+          <Box
+            w="1000px"
+            borderRadius="lg"
+            h="200px"
+            bg="gray.800"
+            mr={4}
+          ></Box>
+          <VStack spacing={2} align="stretch" p={4}>
+            <Text fontSize="md" color="gray.500">
+              Small
             </Text>
-          </Box>
-        </VStack>
+            <Heading fontSize="lg">Hotel Title</Heading>
+            <Text isTruncated noOfLines={2}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Architecto cumque laboriosam, provident tempore dignissimos eaque
+              voluptates dolore incidunt! Pariatur debitis laborum reprehenderit
+              accusamus nulla soluta provident dolorem consequuntur omnis error.
+            </Text>
+            <Box display="flex" pt={4} justifyContent="space-between">
+              <IconPair icon={IoBedOutline}>6</IconPair>
+              <Text color="primary" fontWeight="bold" fontSize="3xl">
+                $6200
+              </Text>
+            </Box>
+          </VStack>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
--- pages/saved/[savedId].tsx
+++ pages/saved/[savedId].tsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { ListDetailsCard } from "@/components/ListDetailsCard";
+import Link from "next/link";
+import { IoChevronBackOutline } from "react-icons/io5";
+import { Heading, IconButton } from "@chakra-ui/react";
 
 const SavedListRoom = () => {
   const router = useRouter();
+  const { savedId } = router.query;
 
   return (
     <div>
+      <IconButton
+        _hover={{ background: "gray.100" }}
+        aria-label="Back Button"
+        padding={4}
+        borderRadius="full"
+        mb={8}
+        mt={4}
+        background="white"
+        icon={<IoChevronBackOutline size="40px" />}
+        onClick={() => router.push("/saved")}
+      />
+      <Heading fontSize="3xl" my={4} mx={4}>
+        List Name
+      </Heading>
       <ListDetailsCard />
     </div>
   );
